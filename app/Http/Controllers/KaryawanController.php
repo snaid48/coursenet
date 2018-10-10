d973f38994aa2eb90a9defa2ab2f8af94bd87d3e
snaid48 <diansulistyadi92@gmail.com>
2018-10-11 00:37:23 +0700

    pertemuan 5

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class KaryawanController extends Controller
 {
@@ -13,7 +14,8 @@ class KaryawanController extends Controller
      */
     public function index()
     {
-        return view('karyawan.home');
+        $karyawan = DB::table('karyawan')->get();
+        return view('karyawan.home',['data'=>$karyawan]);
     }
 
     /**
